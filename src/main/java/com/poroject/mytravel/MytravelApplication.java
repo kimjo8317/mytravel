268ab46a9af11bc8960ddea0dc20ebb375f6f5b7
kimjunoh <kimjo8317@gmail.com>
2023-05-18 01:54:33 +0900

    my travel

---
+++ src/main/java/com/poroject/mytravel/MytravelApplication.java
@@ -0,0 +1,13 @@
+package com.poroject.mytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poroject/mytravel/MytravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poroject.mytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
