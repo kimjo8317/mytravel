391c9bbe108ad4606ed85150d5fd51c606c16aa0
kimjunoh <kimjo8317@gmail.com>
2023-05-18 23:30:24 +0900

    게시판테이블생성 및 db연결

--- src/main/java/com/poroject/mytravel/controller/BoardController.java
+++ src/main/java/com/poroject/mytravel/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.poroject.mytravel.controller;
 
 import com.poroject.mytravel.model.Board;
 import com.poroject.mytravel.repository.BoardRepository;
+import com.poroject.mytravel.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,10 +17,34 @@ import java.util.List;
 public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    /*아이디값을 조회하여 사용*/
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "/board/form";
+    }
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
