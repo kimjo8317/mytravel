d2fdf96965fa0d40adebfb775e0b346152e7c3cc
kimjunoh <kimjo8317@gmail.com>
2023-05-19 17:58:37 +0900

    board search 기능추가

--- src/main/java/com/poroject/mytravel/repository/BoardRepository.java
+++ src/main/java/com/poroject/mytravel/repository/BoardRepository.java
@@ -1,7 +1,16 @@
 package com.poroject.mytravel.repository;
 
 import com.poroject.mytravel.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title,String content);
+    /*title,content이용 검색*/
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
 }
